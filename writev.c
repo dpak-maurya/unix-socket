d090dfcc73b821e279a253c45cc30d4bfe677299
dpak-maurya <dpak.m932@gmail.com>
2020-03-01 18:30:27 +0530

    writev commit

--- writev.c
+++ writev.c
@@ -4,40 +4,27 @@
 #include <stdlib.h>
 #include<sys/types.h>
 #include<fcntl.h>
+#include<string.h>
+
 
 int main(int argc, char *argv[])
 {
+	ssize_t bytes_written;
 	int fd;
+	char *buf0 = "short string\n";
+	char *buf1 = "This is a longer string\n";
+	char *buf2 = "This is the longest string in this example\n";
+	int iovcnt;
 	struct iovec iov[3];
-	char myStruct[]="this is cn lab .writing code for writev\n";	/* First buffer */
-	int x=10;
-	/* Second buffer */
-	#define STR_SIZE 100
-	char str[STR_SIZE]="today thsi dfslkj fdaskljf afds  fadsklj adfs\n";
-	/* Third buffer */
-	ssize_t numRead, totRequired;
-	fd = open(argv[1], O_RDONLY);
-	
-	totRequired = 0;
 
-	iov[0].iov_base = myStruct;
-	iov[0].iov_len = sizeof(myStruct);
-	totRequired += iov[0].iov_len;
-	iov[1].iov_base = &x;
-	iov[1].iov_len = sizeof(x);
-	totRequired += iov[1].iov_len;
-	iov[2].iov_base = str;
-	iov[2].iov_len = STR_SIZE;
-	totRequired += iov[2].iov_len;
+	iov[0].iov_base = buf0;
+	iov[0].iov_len = strlen(buf0);
+	iov[1].iov_base = buf1;
+	iov[1].iov_len = strlen(buf1);
+	iov[2].iov_base = buf2;
+	iov[2].iov_len = strlen(buf2);
 
+	iovcnt = sizeof(iov) / sizeof(struct iovec);
 
-	numRead = writev(fd, iov, 3);
-	
-	if (numRead < totRequired)
-	printf("write fewer bytes than requested\n");
-	printf("total bytes requested: %ld; bytes read: %ld\n",
-	(long) totRequired, (long) numRead);
-	for(int i=0;i<3;i++)
-		printf("%s \n",iov[i].iov_base );
-	exit(EXIT_SUCCESS);
+	bytes_written = writev(fd, iov, iovcnt);
 }
